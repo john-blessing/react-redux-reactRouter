1a8eeb079d68c11e999c642578874ed001ba70b8
john <1585185302@qq.com>
2018-02-13 08:17:38 +0800

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import App from '../plugins/App'
+
+import Sidebar from '../components/Sidebar'
+import About from '../plugins/about/About'
+import Home from '../plugins/home/Home'
+
+import { Layout } from 'antd';
 import mystore from '../store'
 import { createStore } from 'redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -11,7 +16,12 @@ export default class Myrouter extends React.Component {
     render(){
         return (<Provider store={store}>
                     <Router>
-                        <Route path="/" component={App} />
+                        <Layout style={{ minHeight: '100vh' }}>
+                          <Sidebar />
+                          <Route exact path='/' component={Home}/>
+                          <Route path="/home" component={Home} />
+                          <Route path="/about" component={About} />
+                        </Layout>
                     </Router>
                 </Provider>)
     }
