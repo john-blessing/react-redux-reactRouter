cce4a415641349cef956590199619d069d505546
john <1585185302@qq.com>
2018-02-14 17:46:43 +0800

    commit

---
+++ src/api/config.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const baseURL = 'https://some-domain.com/api/'
+
+const api = axios.create({
+    baseURL,
+    timeout: 5000,
+    withCredentials: true
+});
+
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Do something with response data
+    return response;
+}, function (error) {
+    // Do something with response error
+    return Promise.reject(error);
+});
+
+export default api;
