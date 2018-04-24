a93c82eca454b83035498b79bc0231bc3408da37
keife08 <1585185302@qq.com>
2018-04-24 13:48:26 +0800

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
 
 import React from 'react'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import mystore from '../store'
-import {createStore, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
-import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 
 import asyncComponent from '../components/AsyncComponent';
-import {Layout} from 'antd';
+import { Layout } from 'antd';
 import Sidebar from '../components/sidebar/Sidebar';
 const Home = asyncComponent(() => import('../plugins/home/Home'));
 const Error = asyncComponent(() => import('../plugins/error/Error'));
@@ -23,21 +23,28 @@ if (process.env.NODE_ENV !== 'production') {
 
 const store = createStore(mystore, applyMiddleware(...middleware))
 
+const routes = [{
+    path:'/',
+    component: Home
+},{
+    path: '/home',
+    component: Home
+}, {
+    path: '/about',
+    component: About
+}]
 export default class Myrouter extends React.Component {
     render() {
-        return ( <Provider store = {store}> 
-                    <Router> 
-                        <Layout style = {{ minHeight: '100vh'}} > 
-                            <Sidebar / > 
-                            <Switch>
-                                <Route exact path = '/' component = {Home} /> 
-                                <Route path = "/home" component = {Home} /> 
-                                <Route path = "/about/:id" component = {About} ></Route> 
-                                <Route path = "/404" component = {Error} />
-                                <Redirect from="*" to="/404" />
-                            </Switch>
-                        </Layout> 
-                    </Router> 
-                </Provider> )
+        return (<Provider store={store}>
+            <Router>
+                <Layout style={{ minHeight: '100vh' }} >
+                    <Sidebar />
+                    <Switch>
+                        {routes.map((route, i) => <Route exact key={i} path={route.path} component={route.component} />)}
+                        <Route component={Error} />
+                    </Switch>
+                </Layout>
+            </Router>
+        </Provider>)
     }
 }
