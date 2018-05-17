d3e45be6045edc23bd193644ec8d190a29c0c7a9
john-blessing <1585185302@qq.com>
2018-05-17 10:07:46 +0800

    commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,32 +1,15 @@
 
 import React from 'react'
-import Loadable from 'react-loadable';
 import { Provider } from 'react-redux'
 import mystore from '../store'
 import { createStore, applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
-import Loading from '../components/Loading';
+import routes from './routes';
 import Sidebar from '../components/sidebar/Sidebar';
 
 import { Layout } from 'antd';
-
-const Home = Loadable({
-    loader: () => import('../plugins/home/Home'),
-    loading: Loading
-})
-
-const Error = Loadable({
-    loader: () => import('../plugins/error/Error'),
-    loading: Loading
-})
-
-const About = Loadable({
-    loader: () => import('../plugins/about/About'),
-    loading: Loading
-})
 
 const middleware = [thunk];
 if (process.env.NODE_ENV !== 'production') {
@@ -34,17 +17,6 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const store = createStore(mystore, applyMiddleware(...middleware))
-
-const routes = [{
-    path: '/',
-    component: Home
-}, {
-    path: '/home',
-    component: Home
-}, {
-    path: '/about',
-    component: About
-}]
 
 export default class Myrouter extends React.Component {
     render() {
@@ -54,7 +26,6 @@ export default class Myrouter extends React.Component {
                     <Sidebar />
                     <Switch>
                         {routes.map((route, i) => <Route exact key={i} path={route.path} component={route.component} />)}
-                        <Route component={Error} />
                     </Switch>
                 </Layout>
             </Router>
---
+++ src/routes/routes.js
@@ -0,0 +1,33 @@
+import Loadable from 'react-loadable';
+import Loading from '../components/Loading';
+
+const Home = Loadable({
+    loader: () => import('../plugins/home/Home'),
+    loading: Loading
+})
+
+const Error = Loadable({
+    loader: () => import('../plugins/error/Error'),
+    loading: Loading
+})
+
+const About = Loadable({
+    loader: () => import('../plugins/about/About'),
+    loading: Loading
+})
+
+const routes = [{
+    path: '/',
+    component: Home
+}, {
+    path: '/home',
+    component: Home
+}, {
+    path: '/about',
+    component: About
+}, {
+    path: '/error',
+    component: Error
+}]
+
+export default routes
